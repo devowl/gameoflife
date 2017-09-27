528ffeb6a2da88e6dfb4a359e1a968a04d7772a9
SergeyVaulin <SergeyVaulin@localhost>
2017-09-27 20:54:03 +0300

    First commit

---
+++ Gol.Solution/Gol.Core/Controls/Models/MonoLifeGridModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Gol.Core.Controls.Models
+{
+    /// <summary>
+    /// Mono life grid model.
+    /// </summary>
+    public class MonoLifeGridModel
+    {
+        private readonly bool[,] _sourceArray;
+
+        /// <summary>
+        /// Constructor for <see cref="MonoLifeGridModel"/>.
+        /// </summary>
+        public MonoLifeGridModel(bool[,] sourceArray)
+        {
+            if (sourceArray == null)
+            {
+                throw new ArgumentNullException(nameof(sourceArray));
+            }
+
+            _sourceArray = (bool[,])sourceArray.Clone();
+        }
+
+        /// <summary>
+        /// Grid width.
+        /// </summary>
+        public int Width => _sourceArray.GetLength(0);
+
+        /// <summary>
+        /// Grid height.
+        /// </summary>
+        public int Height => _sourceArray.GetLength(1);
+
+        /// <summary>
+        /// Get point black state.
+        /// </summary>
+        /// <param name="x">X coordinate.</param>
+        /// <param name="y">Y coordinate.</param>
+        /// <returns></returns>
+        public bool this[int x, int y] => _sourceArray[x, y];
+    }
+}
---
+++ Gol.Solution/Gol.Core/Controls/Views/MonoLifeView.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using Gol.Core.Controls.Models;
+
+namespace Gol.Core.Controls.Views
+{
+    /// <summary>
+    /// Mono life grid control.
+    /// </summary>
+    public class MonoLifeView : Control
+    {
+        /// <summary>
+        /// Dependency property for <see cref="MonoLifeGridModel"/> property.
+        /// </summary>
+        public static readonly DependencyProperty MonoLifeGridModelProperty;
+
+        /// <summary>
+        /// Static constructor for <see cref="MonoLifeView"/>.
+        /// </summary>
+        static MonoLifeView()
+        {
+            MonoLifeGridModelProperty = DependencyProperty.Register(
+                nameof(MonoLifeGridModel),
+                typeof(MonoLifeGridModel),
+                typeof(MonoLifeView),
+                new PropertyMetadata(default(MonoLifeGridModel)));
+        }
+
+        /// <summary>
+        /// Mono grid model.
+        /// </summary>
+        public MonoLifeGridModel MonoLifeGridModel
+        {
+            get
+            {
+                return (MonoLifeGridModel)GetValue(MonoLifeGridModelProperty);
+            }
+
+            set
+            {
+                SetValue(MonoLifeGridModelProperty, value);
+            }
+        }
+    }
+}
